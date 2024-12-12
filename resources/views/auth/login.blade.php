001d0d96bca13dbf4e1b5eb0f3f73c62b78aa6bb
budiyono <budiyono.dev@gmail.com>
2024-12-12 09:00:32 +0700

    component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,46 +11,6 @@
             </button>
         </div>
     @endif
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark bg-cda-primary">
-        <a class="navbar-brand cda-brand" href="#">Codingduluaja</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <div class="custom-control custom-switch toggle-dark-mode">
-                <input type="checkbox" class="custom-control-input" id="toggleDarkMode">
-                <label class="custom-control-label" for="toggleDarkMode">Dark Mode</label>
-            </div>
-
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
     <div class="container min-vh-100 pt-3">
         <div class="row ">
             <div class="col d-none d-md-block">
--- resources/views/base_view.blade.php
+++ resources/views/base_view.blade.php
@@ -10,6 +10,7 @@
 
 </head>
 <body>
+    <x-navbar/>
     @yield('main')
     <script src="assets/js/jquery-3.7.1.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
